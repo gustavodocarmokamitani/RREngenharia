fc0b103b2329431a5914aa007bf81f8e2016b094
gustavodocarmokamitani <gustavodocarmokamitani@gmail.com>
2022-03-01 08:37:54 -0300

    style(:art:): finishing the responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
+
+
 $('.carousel').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000, dots: true,
-  });
-          
-  AOS.init();
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 1008,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },   
+  ],
+
+});
+
+
+
+AOS.init();
+
+console.log(window.screen.width)
